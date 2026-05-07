76ee44400fde238841173b2ee2cb7fcd66b14f7e
Mayrton777 <mayrtontrabalho@gmail.com>
2026-05-07 00:03:41 -0300

    chore(migrations): gerar migration CorrecaoSeedAdmin
    - Criação dos arquivos de migration para refletir a alteração pendente no model snapshot (EmailConfirmado = true do usuário admin).
    
    - Resolução do erro 'PendingModelChangesWarning' que impedia a atualização do banco de dados na nova máquina.

--- backend/src/Infrastructure/Migrations/20260507024646_CorrecaoSeedAdmin.Designer.cs
+++ backend/src/Infrastructure/Migrations/20260507024646_CorrecaoSeedAdmin.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizGamificado.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using QuizGamificado.Infrastructure.Data;
 namespace QuizGamificado.Infrastructure.Migrations
 {
     [DbContext(typeof(QuizDbContext))]
-    partial class QuizDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260507024646_CorrecaoSeedAdmin")]
+    partial class CorrecaoSeedAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Infrastructure/Migrations/20260507024646_CorrecaoSeedAdmin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizGamificado.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoSeedAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TB_USUARIO",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "EmailConfirmado",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TB_USUARIO",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "EmailConfirmado",
+                value: false);
+        }
+    }
+}
